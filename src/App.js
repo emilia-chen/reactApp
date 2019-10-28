cfbc849ec92f53e944f0a9dcccdbd46797a74aac
chenjiacheng123 <jiacheng.chen@hand-china.com>
2019-10-29 00:03:15 +0800

    exan

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends PureComponent {
+ 
+  constructor(props) {
+     super(props);
+     this.state = {
+      items: [{a:1},{a:2},{a:3}]
+     }
+    console.log('constructor1')
+  }
+
+handleClick = () => {
+const { items } = this.state;
+items.splice(items.length - 1, 1);
+this.setState({ items: [].concat(items) });
+}
+
+
   render() {
-    return (
+   
+    console.log('reder')
+     return (
+   
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+         
          欢迎来到菜鸟教程
-          </p>
+
+         <ul>
+{this.state.items.map((i ,index)=> <li key={index}>{i.a}</li>)}
+        </ul>
+      <button onClick={this.handleClick}>delete</button>
           <a
             className="App-link"
             href="https://reactjs.org"
